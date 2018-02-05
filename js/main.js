22ad1f5c7dc9d96b3665ebb3b1f4b4983bac77b1
Jude Kuti <judekuti@gmail.com>
2018-02-05 07:30:58 -0600

    make Ajax call with client ID and secret included

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-main.js
+$(document).ready(function(){
+  $('#searchUser').on('keyup', function(e){
+    let username = e.target.value;
+// Make request to Github
+    $.ajax({
+      url:'https://api.github.com/user/'+username,
+      data:{
+        client_id:'ea8b9ad48306d1a34ab2',
+        client_secret:'002a4677b32b6059601d4a3d309696c94789d779'
+      }
+
+
+
+});
+});
